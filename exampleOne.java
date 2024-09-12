de9325331df81b9b90a3a5012b8fc2d65829b299
Marcus Mian <145162436+marcusmi4n@users.noreply.github.com>
2024-09-12 09:57:20 +0300

    Update exampleOne.java

--- exampleOne.java
+++ exampleOne.java
@@ -1,4 +1,28 @@
 public class exampleOne{
     public static void main(String[] args){
-        }
+        int a = 10;
+        int b = 5;
+
+        //Addition
+        int sum = a + b;
+
+        //Subtraction
+        int difference = a - b;
+
+        //Multiplication
+        int prdt = a * b;
+
+        //Division
+        int divide = a / b;
+
+        //Modulus
+        int mod = a % b;
+
+        System.out.println("Addition: "+sum);
+        System.out.println("Subtraction: "+difference);
+        System.out.println("Multipication: "+prdt);
+        System.out.println("Division: "+divide);
+        System.out.println("Modulus: "+mod);
+        
+    }
 }

78811c56022c69ec742f195876722a38f0a34c16
Marcus Mian <145162436+marcusmi4n@users.noreply.github.com>
2024-09-13 09:15:25 +0300

    Update Even or Odd.java

--- Even or Odd.java
+++ Even or Odd.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
-    public class Numbers{
-        public static void main(String[] args){
-            Scanner n = new Scanner(System.in);
+
+public class Numbers{
+    public static void main(String[] args){
+        Scanner n = new Scanner(System.in);
+        //Number input line
+        System.out.println("Enter a number:");
+        int nmb = n.nextInt();
+        
+        //Number Identifier
+        if(nmb%2 == 0){
+            System.out.println("NUMBER: "+nmb);
+            System.out.println("STATUS: EVEN");
+        }else{
+            System.out.println("NUMBER: "+nmb);
+            System.out.println("STATUS: ODD");
         }
+        
+        n.close();
     }
+}

b98dab8fcc4e1c49f375bd424de1aa608a8ab872
marcusmi4n <marcuslutas@gmail.com>
2024-09-16 20:24:34 +0300

    VScode editing

--- Even_Odd.java
+++ Even_Odd.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Numbers{
+public class Even_Odd{
     public static void main(String[] args){
         Scanner n = new Scanner(System.in);
         //Number input line

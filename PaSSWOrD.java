a2512ef0f871251021c64f2b6125fb41e3544683
Marcus Mian <145162436+marcusmi4n@users.noreply.github.com>
2024-09-17 13:18:15 +0300

    Update PaSSWOrD.java
    Java program that give 3 attempts to put the correct password

--- PaSSWOrD.java
+++ PaSSWOrD.java
@@ -1,1 +1,22 @@
-
+import java.util.Scanner;
+public class PaSSWOrD{
+  public static void main(String[] args){
+    Scanner pwd = new Scanner(System.in);
+  System.out.println("ENTER PASSWORD:");
+    String paswd = pwd.nextLine();
+    if(paswd == "Marcus"){
+      System.out.println("Welcome");
+    }else{
+      int m = 0;
+      while(m < 3){
+        System.out.println("ENTER PASSWORD:");
+    String paswd = pwd.nextLine();
+      m++;
+      }
+    }
+      pwd.close();
+    }
+}
+    
+      
+    

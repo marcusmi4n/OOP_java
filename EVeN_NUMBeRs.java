5887bcc7f17b68800f68dfcd72f260dccc597d9c
Marcus Mian <145162436+marcusmi4n@users.noreply.github.com>
2024-09-17 12:38:40 +0300

    Create EVeN_NUMBeRs.java
    Displays even numbers 1 - 20

---
+++ EVeN_NUMBeRs.java
@@ -0,0 +1,12 @@
+public class EVeN_NUMBeRs{
+  public static void main(String[] args){
+    int m = 1;
+    System.out.println("     ALL EVEN NUMBERS 1 - 20    ");
+    for (m = 1; m < 20; m++){
+      if(m%2 == 0){
+        System.out.println(m);
+      }
+    }
+  }
+}
+        

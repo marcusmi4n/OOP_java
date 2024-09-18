751f42eee751aaf51b172e750b4f83821ff19ef4
Marcus Mian <145162436+marcusmi4n@users.noreply.github.com>
2024-09-18 20:37:10 +0300

    PaSSWOrD.java

--- PaSSWOrD.java
+++ PaSSWOrD.java
@@ -1,22 +1,27 @@
+
 import java.util.Scanner;
-public class PaSSWOrD{
-  public static void main(String[] args){
-    Scanner pwd = new Scanner(System.in);
-  System.out.println("ENTER PASSWORD:");
-    String paswd = pwd.nextLine();
-    if(paswd == "Marcus"){
-      System.out.println("Welcome");
-    }else{
-      int m = 0;
-      while(m < 3){
-        System.out.println("ENTER PASSWORD:");
-    String paswd = pwd.nextLine();
-      m++;
-      }
-    }
-      pwd.close();
+
+public class PaSSWOrD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String correctPassword = "Marcus";
+        int attempts = 3;
+
+        while (attempts > 0) {
+            System.out.println("ENTER PASSWORD: ");
+            String input = scanner.nextLine();
+
+            if (input.equals(correctPassword)) {
+                System.out.println("SUCCESS!");
+            } else {
+                attempts--;
+                System.out.println("Incorrect Password. Attempts remaining: " + attempts);
+
+            }
+            if (attempts == 0) {
+                System.out.println("Account  blocked.");
+            }
+        }
+        scanner.close();
     }
 }
-    
-      
-    

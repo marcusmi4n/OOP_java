f36aff8df016e85f9380f5535eec1b2a6e8cae95
Marcus Mian <145162436+marcusmi4n@users.noreply.github.com>
2024-09-26 12:16:48 +0300

    Create ArrayExample.java
    Displaying Arrays

---
+++ ArrayExample.java
@@ -0,0 +1,10 @@
+public class ArrayExample{
+
+     public static void main(String []args){
+         int[] marcus = {1, 2, 3, 4, 5};
+         for(int i = 0; i < marcus.length; i++){
+             System.out.println("ELEMENT "+i+" : " + marcus[i]);
+             
+         }
+     }
+}

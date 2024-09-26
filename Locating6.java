b27fc4d3516da3a625887612c833061db489d2a4
Marcus Mian <145162436+marcusmi4n@users.noreply.github.com>
2024-09-26 12:29:09 +0300

    Create Locating6.java
    Matrix

---
+++ Locating6.java
@@ -0,0 +1,13 @@
+
+public class Locating6{
+
+     public static void main(String []args){
+        int[][] lutas={
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+            
+        };
+        System.out.println("ELEMENT :" +lutas[1][2]);
+     }
+}
